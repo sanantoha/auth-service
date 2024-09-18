7dd5109cedbfbc0f5933a411f34295bc1b1f35d3
AntonSakharovAE <anton@alefeducation.com>
2024-09-18 21:02:39 +0400

    refactor code.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # auth-service
+
+### required env vars:
+export AUTH_MONGO_URI=mongodb://localhost:27017
+
+export AUTH_PORT=50051
+
+export AUTH_SECRET={secret for jwt token}
+
+    <secret for jwt token> must be the same for all services
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ mod hash_pwd;
 
 const AUTH_SERVICE_PORT_NAME: &str = "AUTH_PORT";
 
+const MONGO_URI: &str = "AUTH_MONGO_URI";
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     std::env::set_var("RUST_LOG", std::env::var("RUST_LOG").unwrap_or("info".to_owned()));
@@ -25,13 +27,14 @@ async fn main() -> Result<(), Error> {
     let secret = env::var(jwt::SECRET_NAME)
         .map_err(|e| Error::Var { input: jwt::SECRET_NAME, source: e })?;
 
+    let mongo_uri = env::var(MONGO_URI)
+        .map_err(|e| Error::Var { input: MONGO_URI, source: e })?;
+
     let addr = format!("[::1]:{}", port).parse()?;
 
-    let client = Client::with_uri_str("mongodb://localhost:27017").await?;
+    let client = Client::with_uri_str(mongo_uri).await?;
     let user_repository = UserRepository::new(client);
     let auth_service = auth::AuthService::new(user_repository, secret);
-
-
 
     let reflection_service = tonic_reflection::server::Builder::configure()
         .register_encoded_file_descriptor_set(auth::proto::FILE_DESCRIPTOR_SET)
